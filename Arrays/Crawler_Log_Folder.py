ed294761368d9a6943bdee23b6150d0877f2f814
Ahadkhan21 <87846249+Ahadkhan21@users.noreply.github.com>
2024-07-09 21:55:37 -0600

    Problem 1598

---
+++ Arrays/Crawler_Log_Folder.py
@@ -0,0 +1,42 @@
+"""
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+
+"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+"./" : Remain in the same folder.
+"x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+
+Constraints:
+
+1 <= logs.length <= 103
+2 <= logs[i].length <= 10
+logs[i] contains lowercase English letters, digits, '.', and '/'.
+logs[i] follows the format described in the statement.
+Folder names consist of lowercase English letters and digits.
+"""
+
+logs = ["d1/","../","../","../"]
+
+def minOperations(logs) -> int:
+    count = 0
+    for i in logs:
+        if i == "../" and count > 0:
+            count -= 1
+        elif i == "../" and count == 0:
+            continue
+        elif i == "./":
+            continue
+
+        else:
+            count += 1
+
+    return count
+
+print(minOperations(logs))
